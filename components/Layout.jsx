ec1e3155380354b7fa5bd690fcc1c981bd93db92
Aldo FWI <aldo.fwi@gmail.com>
2023-02-13 21:18:34 -0400

    Layout Updated

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-const Layout = () => {
+const Layout = ({ children }) => {
 
   return (
     
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>CAP ⚡ ELEC</title>
+      </Head>
+      <header>
+          <Navbar />
+      </header>
+      <main className="container">
+        { children }
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
 
   )
 }
