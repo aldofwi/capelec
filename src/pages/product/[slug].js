5215e6ad039bab473b6e61a3ddd23b199efd2a75
Aldo FWI <aldo.fwi@gmail.com>
2023-02-15 20:28:02 -0400

    product buttons

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -25,7 +25,7 @@ const ProductDetails = ({ product, products }) => {
                 </div> */
                 }
             </div>
-            <div className="product-details-desc">
+            <div className="product-detail-desc">
                 <h1>{name}</h1>
                 <div className="reviews">
                     <div>
@@ -39,9 +39,32 @@ const ProductDetails = ({ product, products }) => {
                         (20)
                     </p>
                 </div>
-                <h4>Details: </h4>
+                <h4>Details : </h4>
                 <p>{details}</p>
                 <p className="price">${price}</p>
+                <div className="quantity">
+                    <h3>Quantity : </h3>
+                    <p className="quantity-desc">
+                        <span className="minus" onClick="">
+                            <AiOutlineMinus />
+                        </span>
+                        <span className="num">0</span>
+                        <span className="plus" onClick="">
+                            <AiOutlinePlus />
+                        </span>
+                    </p>
+                </div>
+                <div className="buttons">
+                    <button 
+                        type="button"
+                        className="add-to-cart"
+                        onClick="">Add to Cart</button>
+                    <button 
+                        type="button"
+                        className="buy-now"
+                        onClick="">Buy Now</button>
+                </div>
+
             </div>
         </div> 
     </div>
