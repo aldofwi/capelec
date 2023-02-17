3c3bae3a5e74961dfdcd89f705b7ca0064f1cc7a
Aldo FWI <aldo.fwi@gmail.com>
2023-02-17 16:29:35 -0400

    Start Cart

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import { Cart } from './'
+import { useStateContext } from 'context/StateContext'
 import { AiOutlineShopping } from 'react-icons/ai' 
 
 const Navbar = () => {
+
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
     
@@ -11,10 +15,12 @@ const Navbar = () => {
         <Link href="/">Cap ⚡ Elec</Link>
       </p>
 
-      <button type="button" className='cart-icon' onClick="">
+      <button type="button" className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className='cart-item-qty'>1</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+
+      { showCart && <Cart /> }
     </div>
 
   )
